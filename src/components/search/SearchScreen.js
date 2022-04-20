8dd6e115f2767a9daaa24c3b0d2e1a678fba1e64
Carlos Carrete <carretecarlosg@gmail.com>
2022-04-20 01:50:23 -0500

    Added a Search

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 
 export const SearchScreen = () => {
+
+    const [value, handleInputChange] = useForm({
+        searchText : ''
+    })
+
+    const {searchText} = value;
+
+    const handleSearch = (e) =>{
+        e.preventDefault();
+        console.log(searchText);
+    }
+
     return (
-        <div>
-            <h1>SearchScreen</h1>
-        </div>
+        <>
+            <h1>Search a Hero</h1>
+            <div className="row">
+                <div className="col-5">
+                    <h4>Search...</h4>
+                    <hr/>
+
+                    <form onSubmit={handleSearch}>
+                        <input 
+                            type="text"
+                            autoComplete="off"
+                            className="form-control"
+                            name="searchText"
+                            value={searchText}
+                            onChange={handleInputChange}
+                        />
+                        <button className="btn btn-dark w-100 mt-3" type="submit">
+                            Search
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+        </>
     )
 }
